80ca57a04fd61cf5f3b3196c000ca02afcb66918
RushingBlast <96780345+RushingBlast@users.noreply.github.com>
2024-05-11 21:18:32 +0300

    Temporary UI Complete

---
+++ .history/index_20240511210620.py
@@ -0,0 +1,83 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from ui import Ui_MainWindow
+import pyqtgraph as pg
+import numpy as np
+import cv2
+from PyQt5.uic import loadUiType
+# from classes import Image, Features, SIFT
+
+
+ui, _ = loadUiType('main.ui')
+
+
+class FaceRecognizer(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(FaceRecognizer, self).__init__()
+        self.setupUi(self)
+        
+        self.loaded_image = None
+
+        self.plotwidget_set = [self.wgt_img_input, self.wgt_img_output]
+
+        # Create an image item for each plot-widget
+        self.image_item_set = [self.item_input, self.item_output] = [pg.ImageItem() for _ in range(2)]
+
+        self.init_application()
+
+        ############################### Connections ##################################################
+        # Connect Openfile Action to its function
+        self.actionOpen.triggered.connect(lambda: self.open_image(0))
+
+    ##############################################################################################                
+ 
+    ################################# Misc Functions #############################################
+
+    def open_image(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setNameFilter("Images (*.png *.jpg *.bmp *.tif *.jpeg)")
+        file_dialog.setWindowTitle("Open Image File")
+        file_dialog.setFileMode(QFileDialog.ExistingFile)
+        if file_dialog.exec_() == QFileDialog.Accepted:
+            selected_file = file_dialog.selectedFiles()[0]
+            self.load_img_file(selected_file)
+
+    def load_img_file(self, image_path):
+
+        # Loads the image using imread, converts it to RGB, then rotates it 90 degrees clockwise
+        image = cv2.rotate(cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB), cv2.ROTATE_90_CLOCKWISE)
+        self.loaded_image = image
+        self.display_image(self.item_input, self.loaded_image)
+
+    @staticmethod
+    def display_image(image_item, image):
+        image_item.setImage(image)
+        image_item.getViewBox().autoRange()
+
+    def setup_plotwidgets(self):
+        for plotwidget in self.findChildren(pg.PlotWidget):
+            if plotwidget in [self.wgt_img_input, self.wgt_img_output]:
+                # Removes Axes and Padding from all plotwidgets intended to display an image
+                plotwidget.showAxis('left', False)
+                plotwidget.showAxis('bottom', False)
+                plotitem = plotwidget.getPlotItem()
+                plotitem.getViewBox().setDefaultPadding(0)
+            plotwidget.setBackground((25, 30, 40))
+            plotwidget.showGrid(x = True, y= True)
+
+        # Adds the image items to their corresponding plot widgets, so they can be used later to display images
+        for plotwidget, imgItem in zip(self.plotwidget_set, self.image_item_set):
+            plotwidget.addItem(imgItem)
+
+    def setup_checkboxes(self):
+        pass
+
+    def init_application(self):
+        self.setup_plotwidgets()
+        # self.setup_checkboxes()
+
+
+app = QApplication(sys.argv)
+win = FaceRecognizer()
+win.show()
+app.exec()
